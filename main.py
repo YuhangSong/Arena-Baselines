2ddd37d3df36fb157b32e6003097385ca2398895
Yuhang Song <yuhangsong.china@gmail.com>
2019-10-25 23:42:02 +0800

    update comments

--- main.py
+++ main.py
@@ -24,23 +24,33 @@ def main():
         ) for agent_i in range(ma_envs.number_agents)
     ]
 
+    # reset
     for agent_i in range(ma_envs.number_agents):
         ma_envs.sa_envs[agent_i].reset()
 
+    # observe_after_step
     for agent_i in range(ma_envs.number_agents):
         obs = ma_envs.sa_envs[agent_i].get_observe_after_reset()
         agents[agent_i].observe_after_reset(obs)
 
     k = 0
     while True:
+
+        # after each agent take an action. here, the ma_envs is not really step forward, it just records the action
+        # from each single agent
         for agent_i in range(ma_envs.number_agents):
             actions = agents[agent_i].act()
             ma_envs.sa_envs[agent_i].step(actions)
+
+        # step forward with the actions recorded in the last step
         ma_envs.step_sync()
+
+        # observe new observation
         for agent_i in range(ma_envs.number_agents):
             obs, reward, done, info = ma_envs.sa_envs[agent_i].get_observe_after_step(
             )
             agents[agent_i].observe_after_step(obs, reward, done, info)
+
         k += 1
         print('========{}========='.format(k))
 
