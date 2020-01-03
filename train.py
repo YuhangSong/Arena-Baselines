28a799ce07c08514b2584edb9150cb8147ac1080
YuhangSong <YuhangSong.China@gmail.com>
2020-01-03 21:22:54 +0800

    add all

--- train.py
+++ train.py
@@ -4,19 +4,26 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import arena
 import argparse
 import yaml
 import copy
+import os
+import pickle
+import logging
 import utils
+
+import numpy as np
+
 import ray
 from ray.tests.cluster_utils import Cluster
 from ray.tune.resources import resources_to_json
 from ray.tune.tune import _make_scheduler, run_experiments
 
-import arena
+# logger = logging.getLogger(__name__)
+
 
 POLICY_ID_PREFIX = "policy"
-SELFPLAY_POLICY_TO_TRAIN = 0
 
 
 def create_parser():
@@ -42,20 +49,46 @@ def create_parser():
         default="visual_FP",
         type=str,
         help=(
-            "type of the observation; options: "
+            "Type of the observation; options: "
             "vector (low-dimensional vector observation); "
             "visual_FP (first-person visual observation); "
             "visual_TP (third-person visual observation); "
             "obs1-obs2-... (combine multiple types of observations); "
         ))
     parser.add_argument(
-        "--policy-assignment",
+        "--iterations-per-reload",
+        default=1,
+        type=int,
+        help=(
+            "Number of iterations between each reload. "
+            "In each reload, if num_populaton>1, one of the learning agents will be picked up randomly to face each other. "
+            "In each reload, if policy_assignment==selfplay, the only one learning policy will be saved and all playing policies will be reloaded. "
+        ))
+    parser.add_argument(
+        "--num-learning-policies",
         default="independent",
         type=str,
         help=(
-            "multiagent only; how to assign policies to agents; options: "
-            "independent (independent learners); "
-            "self_play (only one agent is learning, the others [1] donot explore or [2] update, but they keep [3] sync weights from the learning policy).; "
+            "How to assign policies to agents. Options are as follows: "
+            "all (all agents are bound to learning policies, one for each. This is also known as independent learner. ); "
+            "x (there are x agents bound to x learning policies, one for each; the other (num_agents-x) agents are bound to playing policies, one for each. Setting x=1 is known as selfplay. ); "
+            "Playing policies donot explore or update, but they keep reloading weights from the current and previous learning policy at each reload. "
+        ))
+    parser.add_argument(
+        "--selfplay-recent-prob",
+        default=0.8,
+        type=float,
+        help=(
+            "In selfplay, the probability of chosing recent model. "
+            "In other cases, it will choose uniformly among historical models. "
+        ))
+    parser.add_argument(
+        "--num-population",
+        default=1,
+        type=int,
+        help=(
+            "Number of learning agents in population-based training. "
+            "In each reload, one of the learning agents will be picked up randomly to face each other. "
         ))
 
     return parser
@@ -80,7 +113,8 @@ def run(args, parser):
                 "local_dir": args.local_dir,
                 "resources_per_trial": (
                     args.resources_per_trial and
-                    resources_to_json(args.resources_per_trial)),
+                    resources_to_json(args.resources_per_trial)
+                ),
                 "stop": args.stop,
                 "config": dict(
                     args.config,
@@ -90,7 +124,10 @@ def run(args, parser):
                         train_mode=args.train_mode,
                         obs_type=args.obs_type,
                     ),
-                    policy_assignment=args.policy_assignment,
+                    iterations_per_reload=args.iterations_per_reload,
+                    num_learning_policies=args.num_learning_policies,
+                    selfplay_recent_prob=args.selfplay_recent_prob,
+                    num_populaton=args.num_populaton,
                 ),
                 "restore": args.restore,
                 "num_samples": args.num_samples,
@@ -102,15 +139,24 @@ def run(args, parser):
     # the default support of grid_search
     grid_experiments = {}
     for experiment_key in experiments.keys():
-        for policy_assignment_item in arena.get_list_from_gridsearch(experiments[experiment_key]["config"]["policy_assignment"]):
-            grid_experiment_key = "{}__policy_assignment={}".format(
-                experiment_key,
-                policy_assignment_item,
-            )
-            grid_experiments[grid_experiment_key] = copy.deepcopy(
-                experiments[experiment_key]
-            )
-            grid_experiments[grid_experiment_key]["config"]["policy_assignment"] = policy_assignment_item
+        for iterations_per_reload_item in arena.get_list_from_gridsearch(experiments[experiment_key]["config"]["iterations_per_reload"]):
+            for num_learning_policies_item in arena.get_list_from_gridsearch(experiments[experiment_key]["config"]["num_learning_policies"]):
+                for selfplay_recent_prob_item in arena.get_list_from_gridsearch(experiments[experiment_key]["config"]["selfplay_recent_prob"], experiments[experiment_key]["config"]["num_learning_policies"] == "all"):
+                    for num_populaton_item in arena.get_list_from_gridsearch(experiments[experiment_key]["config"]["num_populaton"]):
+                        grid_experiment_key = "{}_ipr={}_pa={}_srp={}_np={}".format(
+                            experiment_key,
+                            iterations_per_reload_item,
+                            num_learning_policies_item,
+                            selfplay_recent_prob_item,
+                            num_populaton_item,
+                        )
+                        grid_experiments[grid_experiment_key] = copy.deepcopy(
+                            experiments[experiment_key]
+                        )
+                        grid_experiments[grid_experiment_key]["config"]["iterations_per_reload"] = iterations_per_reload_item
+                        grid_experiments[grid_experiment_key]["config"]["num_learning_policies"] = num_learning_policies_item
+                        grid_experiments[grid_experiment_key]["config"]["selfplay_recent_prob"] = selfplay_recent_prob_item
+                        grid_experiments[grid_experiment_key]["config"]["num_populaton"] = num_populaton_item
 
     experiments = grid_experiments
 
@@ -135,7 +181,7 @@ def run(args, parser):
                 ),
                 env_config=exp["config"]["env_config"],
             )
-            number_agents = dummy_env.number_agents
+            num_agents = dummy_env.number_agents
 
             agent_id_prefix = dummy_env.get_agent_id_prefix()
 
@@ -155,111 +201,136 @@ def run(args, parser):
 
             # create
             # policies: config of policies
+            policies = {}
             # learning_policy_ids: a list of policy ids of which the policy is trained
+            learning_policy_ids = []
             # playing_policy_ids: a list of policy ids of which the policy is not trained
-            if exp["config"]["policy_assignment"] in ["independent"]:
+            playing_policy_ids = []
 
-                # build number_agents independent learning policies
-                policies = {}
-                learning_policy_ids = []
-                for agent_i in range(number_agents):
-                    key_ = get_policy_id(agent_i)
-                    learning_policy_ids += [key_]
-                    policies[key_] = (
-                        None, obs_space, act_space, {})
+            if exp["config"]["num_learning_policies"] == "all":
+                num_learning_policies = num_agents
+            else:
+                num_learning_policies = int(
+                    exp["config"]["num_learning_policies"])
 
-            elif exp["config"]["policy_assignment"] in ["self_play"]:
-
-                policies = {}
-
-                # build just one learning policy
-                key_ = get_policy_id(SELFPLAY_POLICY_TO_TRAIN)
-                learning_policy_ids = [key_]
+            # build learning policies
+            for agent_i in range(num_learning_policies):
+                key_ = get_policy_id(agent_i)
+                learning_policy_ids += [key_]
                 policies[key_] = (
                     None, obs_space, act_space, {}
                 )
 
-                # build all other policies as playing policy
+            # build playing policy
 
-                # build custom_action_dist to be playing mode dist (no exploration)
-                # TODO: support pytorch policy and other algorithms, currently only add support for tf_action_dist on PPO
-                # see this issue for a fix: https://github.com/ray-project/ray/issues/5729
+            # build custom_action_dist to be playing mode dist (no exploration)
+            # TODO: support pytorch policy and other algorithms, currently only add support for tf_action_dist on PPO
+            # see this issue for a fix: https://github.com/ray-project/ray/issues/5729
 
-                if exp["run"] not in ["PPO"]:
-                    raise NotImplementedError
+            if exp["run"] not in ["PPO"]:
+                raise NotImplementedError
 
-                if act_space.__class__.__name__ == "Discrete":
+            if act_space.__class__.__name__ == "Discrete":
 
-                    from ray.rllib.models.tf.tf_action_dist import Categorical
-                    from ray.rllib.utils.annotations import override
+                from ray.rllib.models.tf.tf_action_dist import Categorical
+                from ray.rllib.utils.annotations import override
 
-                    class DeterministicCategorical(Categorical):
-                        """Deterministic version of categorical distribution for discrete action spaces."""
+                class DeterministicCategorical(Categorical):
+                    """Deterministic version of categorical distribution for discrete action spaces."""
 
-                        @override(Categorical)
-                        def _build_sample_op(self):
-                            return tf.squeeze(tf.argmax(self.inputs, 1), axis=1)
+                    @override(Categorical)
+                    def _build_sample_op(self):
+                        return tf.squeeze(tf.argmax(self.inputs, 1), axis=1)
 
-                    custom_action_dist = DeterministicCategorical
+                custom_action_dist = DeterministicCategorical
 
-                elif act_space.__class__.__name__ == "Box":
+            elif act_space.__class__.__name__ == "Box":
 
-                    from ray.rllib.models.tf.tf_action_dist import Deterministic
-                    custom_action_dist = Deterministic
-
-                else:
-
-                    raise NotImplementedError
-
-                # build all other policies as playing policy
-                playing_policy_ids = []
-                for agent_i in range(1, number_agents):
-                    key_ = get_policy_id(agent_i)
-                    playing_policy_ids += [key_]
-                    policies[key_] = (
-                        None, obs_space, act_space, {
-                            "custom_action_dist": custom_action_dist
-                        }
-                    )
+                from ray.rllib.models.tf.tf_action_dist import Deterministic
+                custom_action_dist = Deterministic
 
             else:
+
                 raise NotImplementedError
+
+            # build all other policies as playing policy
+            for agent_i in range(num_learning_policies, num_agents):
+                key_ = get_policy_id(agent_i)
+                playing_policy_ids += [key_]
+                policies[key_] = (
+                    None, obs_space, act_space, {
+                        "custom_action_dist": custom_action_dist
+                    }
+                )
 
             # create
             # policy_mapping_fn: a map from agent_id to policy_id
-            if exp["config"]["policy_assignment"] in ["independent", "self_play"]:
 
-                # create policy_mapping_fn that maps agent i to policy i, so called policy_mapping_fn_i2i
-                def policy_mapping_fn_i2i(agent_id):
-                    return get_policy_id(get_agent_i(agent_id))
+            # create policy_mapping_fn that maps agent i to policy i, so called policy_mapping_fn_i2i
+            def policy_mapping_fn_i2i(agent_id):
+                return get_policy_id(get_agent_i(agent_id))
 
-                # use policy_mapping_fn_i2i as policy_mapping_fn
-                policy_mapping_fn = policy_mapping_fn_i2i
-
-            else:
-                raise NotImplementedError
+            # use policy_mapping_fn_i2i as policy_mapping_fn
+            policy_mapping_fn = policy_mapping_fn_i2i
 
             # create
             # on_train_result: a function called after each trained iteration
-            if exp["config"]["policy_assignment"] in ["independent"]:
 
-                on_train_result = None
+            input(exp["config"]["iterations_per_reload"])
 
-            elif exp["config"]["policy_assignment"] in ["self_play"]:
+            def on_train_result(info):
+                if info["result"]["training_iteration"] % exp["config"]["iterations_per_reload"] == 0:
+                    print(
+                        "Reload playing policies and save learning policy.")
+                    print(info["trainer"].logdir)
 
-                if len(learning_policy_ids) != 1:
-                    raise Exception(
-                        "In self_play, there can be one and only one learning policy")
+                    def get_checkpoint_path(population_i=None):
 
-                def on_train_result(info):
-                    for playing_policy_id in playing_policy_ids:
-                        info["trainer"].get_policy(playing_policy_id).set_weights(
-                            info["trainer"].get_policy(
-                                learning_policy_ids[0]
-                            ).get_weights()
+                        if population_i is None:
+                            population_i = np.random.randint(
+                                exp["config"]["num_populaton"]
+                            )
+
+                        checkpoint_path = os.path.join(
+                            info["trainer"].logdir,
+                            "learning_agent-p_{}-i_{}".format(
+                                population_i,
+                                info["trainer"].iteration,
+                            )
                         )
-            else:
-                raise NotImplementedError
+
+                        return checkpoint_path
+
+                    for policy_id in learning_policy_ids:
+                        checkpoint_path = get_checkpoint_path()
+                        try:
+                            pickle.dump(
+                                info["trainer"].get_policy(
+                                    policy_id
+                                ).get_weights(),
+                                open(checkpoint_path, "wb")
+                            )
+                        except Exception as e:
+                            print("Save learning agent p_{} i_{} failed: {}".format(
+                                population_i,
+                                info["trainer"].iteration,
+                                e,
+                            ))
+
+                    for policy_id in (learning_policy_ids + playing_policy_ids):
+                        checkpoint_path = get_checkpoint_path()
+                        try:
+                            info["trainer"].get_policy(
+                                policy_id
+                            ).set_weights(
+                                pickle.load(open(checkpoint_path, "rb"))
+                            )
+                        except Exception as e:
+                            print("Load learning agent p_{} i_{} failed: {}".format(
+                                population_i,
+                                info["trainer"].iteration,
+                                e,
+                            ))
 
             # generate multiagent part of the config
             if "multiagent" in exp["config"].keys():
@@ -277,14 +348,17 @@ def run(args, parser):
             if "callbacks" in exp["config"].keys():
                 input("# WARNING: Override")
 
-            exp["config"]["callbacks"] = {
-                "on_train_result": ray.tune.function(
-                    on_train_result
-                ),
-            }
+            # exp["config"]["callbacks"] = {
+            #     "on_train_result": ray.tune.function(
+            #         on_train_result
+            #     ),
+            # }
 
         # del customized configs, as these configs have been reflected on other configs
-        del exp["config"]["policy_assignment"]
+        del exp["config"]["iterations_per_reload"]
+        del exp["config"]["num_learning_policies"]
+        del exp["config"]["selfplay_recent_prob"]
+        del exp["config"]["num_populaton"]
 
     # config ray cluster
     if args.ray_num_nodes:
